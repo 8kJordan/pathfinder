3a979fe1300387f07b550beb965577956179024b
8kJordan <thepro321leyskil@gmail.com>
2025-10-08 00:39:57 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+
+fn test_arithmetic(a: i32, b: i32) -> i32{
+    a + b
+}
+
+
+fn main() {
+    println!("Hello, world!");
+
+    let result = test_arithmetic(1, 2);
+
+    println!("{result}")
+}
